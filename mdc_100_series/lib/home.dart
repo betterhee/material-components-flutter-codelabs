67eda22e40efb1458755db01ec873af4a1ff3e9c
홍다희 <dahee.hong@seeroo.co.kr>
2023-06-19 17:28:06 +0900

    feat: Add collection of cards

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -13,11 +13,76 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/number_symbols_data.dart';
+
+import 'model/product.dart';
+import 'model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  // TODO: Make a collection of cards (102)
+  // Make a collection of cards (102)
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString(),
+    );
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start, // 텍스트를 앞 가장자리에 정렬
+          children: [
+            AspectRatio(
+              // AspectRatio 위젯은 제공되는 이미지의 종류와 상관없이 이미지의 모양을 결정
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // Adjust the box size (102)
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+                  children: [
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product.name,
+                      style: theme.textTheme.titleLarge,
+                      maxLines: 1,
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.titleSmall,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -63,35 +128,8 @@ class HomePage extends StatelessWidget {
         crossAxisCount: 2, // 전체 항목 수
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        // TODO: Build a grid of cards (102)
-        children: [
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // 텍스트를 앞 가장자리에 정렬
-              children: [
-                AspectRatio(
-                  // AspectRatio 위젯은 제공되는 이미지의 종류와 상관없이 이미지의 모양을 결정
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Title'),
-                      const SizedBox(
-                        height: 8.0,
-                      ),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
+        // Build a grid of cards (102)
+        children: _buildGridCards(context),
       ),
       // Set resizeToAvoidBottomInset (101)
       // 키보드의 모양으로 홈페이지나 홈페이지 위젯의 크기가 변경되지 않도록 설정
